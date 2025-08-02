d7e14c6dfd76f6499e0e53fd25d45575ef715d45
Tobias Borges <tobias.borgesguimaraes@gmail.com>
2025-08-02 16:15:23 -0300

    modified main.py and convert_files.py, now the functions inside this second file will be called inside main.py

--- convert_files.py
+++ convert_files.py
@@ -1,43 +1,47 @@
 import pandas as pd 
 import numpy as np 
-import sys
-
-# Gets the files names to be converted 
-args = sys.argv 
-
-if len(args) > 2:
-    raise "file convert_files.py must be called with one argument only"
-else:
-    file_name = args[1]
-
-data_path = f'DATA/{file_name}.txt'
-results_path = f'RESULTS/{file_name}_results.txt'
-
-data = pd.read_table(data_path, header=26, sep='       ', names=['2theta', 'intensity'], engine='python')
-size = len(data)
 
 
-with open(results_path, 'r') as file:
-    content = file.read()[3:] # The indexing is here to disconsider the '#  ' at the beggining of posterior_sample.txt
-    lines = content.splitlines()
 
-    listColumnNames = lines[0].split(sep=', ')
-    listNumbers = [float(n) for n in lines[-1].split()]
+def convert_results_to_csv(file_name):
+    
+    data_path = f'DATA/{file_name}.txt'
+    results_path = f'RESULTS/{file_name}_results.txt'
+
+    data = pd.read_table(data_path, header=26, sep='       ', names=['2theta', 'intensity'], engine='python')
+    size = len(data)
+
+    with open(results_path, 'r') as file:
+        content = file.read()[3:] # The indexing is here to disconsider the '#  ' at the beggining of posterior_sample.txt
+        lines = content.splitlines()
+
+        listColumnNames = lines[0].split(sep=', ')
+        listNumbers = [float(n) for n in lines[-1].split()]
 
 
-my_dict = dict(zip(listColumnNames, listNumbers))
-results_single_line = pd.DataFrame(my_dict, [0])
+    my_dict = dict(zip(listColumnNames, listNumbers))
+    results_single_line = pd.DataFrame(my_dict, [0])
 
-# Finally put the info inside posterior_sample.txt into our dataFrame
-data['bg'] = [results_single_line.at[0, f'bg[{i}]'] for i in range(size)]
-data['wide'] = [results_single_line.at[0, f'wide[{i}]'] for i in range(size)]
-data['narrow'] = [results_single_line.at[0, f'narrow[{i}]'] for i in range(size)]
-data['model_curve'] = [results_single_line.at[0, f'model_curve[{i}]'] for i in range(size)]
+    # Finally put the info inside posterior_sample.txt into our dataFrame
+    data['bg'] = [results_single_line.at[0, f'bg[{i}]'] for i in range(size)]
+    data['wide'] = [results_single_line.at[0, f'wide[{i}]'] for i in range(size)]
+    data['narrow'] = [results_single_line.at[0, f'narrow[{i}]'] for i in range(size)]
+    data['model_curve'] = [results_single_line.at[0, f'model_curve[{i}]'] for i in range(size)]
+
+    # Save the data to a CSV file
+    data_output_path = f'RESULTS/{file_name}_results.csv'
+    data.to_csv(data_output_path, index=False)
 
 
-# Save the data to a CSV file
-data_output_path = f'{file_name}_results.csv'
-data.to_csv(data_output_path, index=False)
+
+def convert_parameters_to_json(file_name):
+    pass
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,35 +1,56 @@
 import numpy as np 
 import pandas as pd 
 import sys 
-import os
 import yaml
+import shutil
 
+
+
+# This file should be executed along with the name of the file to be analysed, so to do it with the 'easy_data.txt' file, run 'uv run main.py easy_data', or 'python main.py easy_data'
 
 args = sys.argv
-print(args)
 if len(args) != 2:
     raise "This file should be called with a single argument (the name of the file to be analysed)"
 else:
     file_name = args[1]
 
 
-# copy the data file to the amorph directory
-os.system(f"cp DATA/{file_name}.txt amorph/src")
+
+# Copy the data file to the amorph directory
+
+shutil.copy2(f'DATA/{file_name}.txt', 'amorph/src')
 
 
 
+# Edit the config.yaml file so that amorph can analyse the correct file
 
 config_path = 'amorph/src/config.yaml'
 with open(config_path, 'r') as f:
-    config = yaml.load(f, Loader=yaml.SafeLoader)
-    
+    # Load config.yaml file
+    config = yaml.safe_load(f)
+
+    # Apply the changes to the file
     config['data_file'] = f'{file_name}.txt'
+
 with open(config_path, 'w') as f:
-    yaml.dump(config, f, default_flow_style=False, sort_keys=False)
+    # Save the changes to config.yaml
+    yaml.dump(config, f)
 
 
 
+# Run AMORPH.exe
 
+
+
+# Convert the results files to csv and json
+
+
+
+# Save results file to the RESULTS directory
+
+
+
+# Run the analysis on the results file
 
 
 
