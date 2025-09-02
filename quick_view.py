888e866606ed4a99b7d04eb3c0a73dbebbb2bb71
Tobias Borges <tobias.borgesguimaraes@gmail.com>
2025-09-02 08:46:54 -0300

    added a file for quick view of the data, before analysing

---
+++ quick_view.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+def get_points(file_name: str) -> pd.DataFrame:
+    
+    data_path = f'DATA/{file_name}.txt'
+
+    return pd.read_table(data_path, header=26, sep='       ', names=['2theta', 'intensity'], engine='python')
+
+if __name__ == "__main__":
+
+    # Get the points from the txt file
+    file_name = sys.argv[1]
+    data = get_points(file_name)
+
+    # Plot the data
+    x_axis = data['2theta']
+    y_axis = data['intensity']
+    plt.scatter(x_axis, y_axis, label='Data', s=0.5, color='black')
+
+    # Plot info
+    plt.xlabel("$2\\theta$ (degrees)", fontsize=14)
+    plt.ylabel("Intensity (cps)", fontsize=14)
+    plt.legend(fontsize=14)
+    plt.title(f'XRD Analysis - Sample {file_name}', fontsize=16)
+    # plt.grid(True)
+    plt.xlim(0, 85)
+    plt.ylim(0, max(y_axis) * 1.05)
+
+ 
+    # Show the data
+    plt.show()
+
